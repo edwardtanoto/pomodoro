3c54a07fa3e5a0d8667dc7ca7a3fa57515f9c35f
Edward Tanoto <ed27tanoto@gmail.com>
2019-01-29 14:01:57 +0700

    Updates

---
+++ precache-manifest.392c27159496a13adf07ea296e79c84e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/pomodoro/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "77bff3409d81aaf5eaa2",
+    "url": "/pomodoro/static/css/main.a5466e48.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/pomodoro/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "02b13cd1c41a3a59d577",
+    "url": "/pomodoro/static/js/1.02b13cd1.chunk.js"
+  },
+  {
+    "revision": "a702e67ff471c6ecfc33",
+    "url": "/pomodoro/static/js/runtime~main.a702e67f.js"
+  },
+  {
+    "revision": "77bff3409d81aaf5eaa2",
+    "url": "/pomodoro/static/js/main.77bff340.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/pomodoro/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/pomodoro/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/pomodoro/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "02b13cd1c41a3a59d577",
+    "url": "/pomodoro/static/css/1.af14ecae.chunk.css"
+  },
+  {
+    "revision": "7e7c05fe96c2150762d3a2d3718263aa",
+    "url": "/pomodoro/index.html"
+  }
+];
